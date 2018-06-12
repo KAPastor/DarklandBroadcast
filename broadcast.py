01f990af34528ab9940cb5f7c1dbfa71a0797a1a
KAPastor <kyleanthonypastor@gmail.com>
2018-06-12 19:20:24 -0400

    lol

--- broadcast.py
+++ broadcast.py
@@ -5,39 +5,68 @@
 
 import subprocess
 from InstagramAPI import InstagramAPI
+import os
+
+with open('./login.txt', 'r') as myfile:
+    LOGIN = myfile.read().replace('\n', '')
+LOGIN = [x.strip() for x in LOGIN.split(',')]
+USERNAME = LOGIN[0]
+PASSWORD = LOGIN[1]
 
 
-USERNAME = 'darklandbroadcast'
-PASSWORD = ''
-FILE_PATH = './video.mp4'
-PUBLISH_TO_LIVE_FEED = False
-SEND_NOTIFICATIONS = False
+MEDIA_FOLDER = './Media/TheSimpsons/S01'
+
+# Get the caption:
+with open(MEDIA_FOLDER + '/Caption.txt', 'r') as myfile:
+    CAPTION = myfile.read().replace('\n', '')
+with open(MEDIA_FOLDER + '/hashtags.txt', 'r') as myfile:
+    HASHTAGS = myfile.read().replace('\n', '')
+
+# Get the episode list:
+episode_list = [];
+for file in os.listdir(MEDIA_FOLDER):
+    if file.endswith(".avi"):
+        episode_list.append(os.path.join(MEDIA_FOLDER, file))
 
 api = InstagramAPI(USERNAME, PASSWORD, debug=False)
 assert api.login()
+iteration = 1
+# Loop over all of the episode_list
+while True:
+    for episode in episode_list:
+        full_episode_caption = "ITERATION: " + str(iteration) + " \\\ " + CAPTION + " " + episode + " " + HASHTAGS
+        # Post the update:
+        photo_path = MEDIA_FOLDER + '/post.jpg'
+        api.uploadPhoto(photo_path, caption=full_episode_caption)
 
-# first you have to create a broadcast - you will receive a broadcast id and an upload url here
-assert api.createBroadcast()
-broadcast_id = api.LastJson['broadcast_id']
-upload_url = api.LastJson['upload_url']
+        # Start up the broadcast
+        FILE_PATH = episode
+        PUBLISH_TO_LIVE_FEED = False
+        SEND_NOTIFICATIONS = False
 
-# we now start a boradcast - it will now appear in the live-feed of users
-assert api.startBroadcast(broadcast_id, sendNotification=SEND_NOTIFICATIONS)
-ffmpeg_cmd = "ffmpeg -rtbufsize 256M -re -i '{file}' -vf 'transpose=1' -acodec libmp3lame -ar 44100 -b:a 256k -pix_fmt yuv420p -profile:v baseline -s 720x1280 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -g 30 -r 30 -f flv '{stream_url}'".format(
-    file=FILE_PATH,
-    stream_url=upload_url.replace(':443', ':80', ).replace('rtmps://', 'rtmp://'),
-)
+        assert api.createBroadcast()
+        broadcast_id = api.LastJson['broadcast_id']
+        upload_url = api.LastJson['upload_url']
 
-print("Hit Ctrl+C to stop broadcast")
-try:
-    subprocess.call(ffmpeg_cmd, shell=True)
-except KeyboardInterrupt:
-    print('Stop Broadcasting')
+        # we now start a boradcast - it will now appear in the live-feed of users
+        assert api.startBroadcast(broadcast_id, sendNotification=SEND_NOTIFICATIONS)
+        ffmpeg_cmd = "ffmpeg -rtbufsize 256M -re -i '{file}' -vf 'transpose=1' -acodec libmp3lame -ar 44100 -b:a 256k -pix_fmt yuv420p -profile:v baseline -s 720x1280 -bufsize 6000k -vb 400k -maxrate 1500k -deinterlace -vcodec libx264 -preset veryfast -g 30 -r 30 -f flv '{stream_url}'".format(
+            file=FILE_PATH,
+            stream_url=upload_url.replace(':443', ':80', ).replace('rtmps://', 'rtmp://'),
+        )
 
-assert api.stopBroadcast(broadcast_id)
+        print("Hit Ctrl+C to stop broadcast")
+        try:
+            subprocess.call(ffmpeg_cmd, shell=True)
+        except KeyboardInterrupt:
+            print('Stop Broadcasting')
 
-print('Finished Broadcast')
+        assert api.stopBroadcast(broadcast_id)
 
-if PUBLISH_TO_LIVE_FEED:
-    api.addBroadcastToLive(broadcast_id)
-    print('Added Broadcast to LiveFeed')
+        print('Finished Broadcast')
+
+        if PUBLISH_TO_LIVE_FEED:
+            api.addBroadcastToLive(broadcast_id)
+            print('Added Broadcast to LiveFeed')
+
+    iteration = iteration + 1
